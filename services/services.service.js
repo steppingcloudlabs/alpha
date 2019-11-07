a76d951ca8deeb7e7d007c2897e13a09fee8f749
maaz1996 <maaz.abdullah111@gmail.com>
2019-11-07 18:10:05 +0530

    -service attach tenant added

--- services/services.service.js
+++ services/services.service.js
@@ -1,11 +1,24 @@
+const mongodb = require('mongodb');
 module.exports = () => {
   const attachTenantService = (payload, logger, db) => new Promise(async (resolve, reject) => {
     try {
       /**
              * create collections to database and send the status with collection names
              */
-      resolve(payload);
-      logger.info(`Successfully ttached service ${payload}`);
+      const client = new mongodb.MongoClient(`mongodb://${payload.dbhost}:${payload.dbport}`, { useUnifiedTopology: true });
+      client.connect((err) => {
+        if (!err) {
+          logger.info(`Successfully created connection on Mongodb for DatabaseName: ${payload.dbname}`);
+        }
+      });
+      const db = client.db(payload.dbname);
+      const collectNames = payload.collectionName
+      const size = collectNames.length
+      for (let i = 0; i < size; i++) {
+        db.createCollection(collectNames[i]);
+      }
+      resolve(collectNames);
+      logger.info(`Successfully attached service ${payload}`);
     } catch (error) {
       reject(error);
       logger.error(`Error while attaching service ${payload}`);
