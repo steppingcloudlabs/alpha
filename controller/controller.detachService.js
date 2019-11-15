de49365040d1949c5e421bbd26464fdc485249fa
Prakritidev Verma <prakritidevverma@gmail.com>
2019-11-15 11:44:33 +0530

    feat: added /POST json validation in detachservice API

--- controller/controller.detachService.js
+++ controller/controller.detachService.js
@@ -1,24 +1,56 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-await-in-loop */
 const servicesService = require('../services/detachServices.service')();
-
+const JsonValidator = require('jsonschema').Validator;
+const validatator = new JsonValidator();
 module.exports = () => {
     const detachTenantService = async(req, res, next, { logger }) => {
         try {
             const payload = req.body;
-            const response = await servicesService.detachTenantService(payload, logger);
-            if (response) {
-                res.status(200).send({
-                    status: '200 OK',
-                    result: response,
-                });
-                logger.info('Successfully created SFTP logical saperation');
+            /**
+             * /POST body Validation
+             */
+
+            const detachTenantServiceSchema = {
+                id: '/detachTenantServiceSchema',
+                type: 'object',
+                properties: {
+                    company_id: { type: 'string', },
+                    service_name: { type: 'string' },
+                    status: { type: { "service_status": 'boolean' } },
+                },
+                required: ['service_name', 'company_id', 'status'],
+            };
+            validatator.addSchema(detachTenantServiceSchema, '/detachTenantServiceSchema');
+            const validatorResponse = (validatator.validate(payload, '/detachTenantServiceSchema')).valid;
+            if (validatorResponse) {
+                const response = await servicesService.detachTenantService(payload, logger);
+                if (response) {
+                    res.status(200).send({
+                        status: '200 OK',
+                        result: response,
+                    });
+                    logger.info('Successfully created SFTP logical saperation');
+                } else {
+                    res.status(200).send({
+                        status: '400',
+                        result: 'Error while created SFTP server',
+                    });
+                    logger.error('Error while created SFTP logical saperation');
+                }
             } else {
                 res.status(200).send({
                     status: '400',
-                    result: 'Error while created SFTP server',
-                });
-                logger.error('Error while created SFTP logical saperation');
+                    result: 'Not a valid JSON, checkout help for json schema ',
+                    help: {
+                        company_id: { type: 'string', required: true },
+                        service_name: { type: 'string', required: true },
+                        status: {
+                            type: { "service_status": 'boolean' },
+                            required: true
+                        }
+                    }
+                })
             }
         } catch (error) {
             next(error);
