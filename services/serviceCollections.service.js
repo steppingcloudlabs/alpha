18e0404fa29bf7410d6ea441bcc11a16ae813a6d
Prakritidev Verma <prakritidevverma@gmail.com>
2020-01-10 12:48:51 +0530

    hot-fix: corrected destructuring of payload for /addService API

--- services/serviceCollections.service.js
+++ services/serviceCollections.service.js
@@ -2,8 +2,8 @@ const serviceColletionModel = require('../model/servicesCollectionSchema')
 module.exports = () => {
     const addService = (payload, logger, db) => new Promise(async(resolve, reject) => {
         try {
-            const { serviceName, collection } = payload.body;
-            const collectionSchema = new serviceColletionModel({ serviceName, collection })
+            const { serviceName, collections } = payload;
+            const collectionSchema = new serviceColletionModel({ "service_name": serviceName, "collection_name": collections })
             await collectionSchema.save();
             resolve(" Saved Collection Names of the service")
         } catch (error) {
